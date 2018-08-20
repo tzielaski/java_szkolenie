58cf1ea9bad967c1835bbe8a716f23399e75b76e
tzielaski <ziolo320t@gmail.com>
2018-08-20 11:07:09 +0200

    project_maven 1.0

---
+++ project_maven/src/test/java/pl/com/sages/hello/AppTest.java
@@ -0,0 +1,20 @@
+package pl.com.sages.hello;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
